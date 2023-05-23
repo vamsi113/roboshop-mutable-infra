4d904d32e1ef2979486c97ca30da05abb88ff9ff
vamsi113 <krishultimate5@gmail.com>
2023-05-22 20:37:22 -0400

    change

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 module "vpc" {
   source   = "./vendor/modules/vpc"
-  vpc_cidr = var.vpc_cidr
+  vpc      = var.vpc
+  env      = var.env
 }
+
+## here module will take care how to iterate and this way main code i am keeping minimal and will enhance module with other things
