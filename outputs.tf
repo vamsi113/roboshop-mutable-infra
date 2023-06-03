7695ad1731132a910342571feb124f75c7e15c50
vamsi113 <krishultimate5@gmail.com>
2023-06-02 20:39:25 -0400

    change

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "vpc" {
   value = module.vpc.vpc
 }
 
-output "subnets" {
-  value = module.vpc.subnets
+#output "subnets" {
+#  value = module.vpc.subnets
+#}
+
+
+output "route_tables" {
+  value = module.vpc.route_tables
 }
