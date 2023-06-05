42de45b40885532f2d8c6ee67bf17405bc31c90c
vamsi113 <krishultimate5@gmail.com>
2023-06-05 12:49:54 -0400

    change

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  private_subnets =lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null),"app", null), "subnets",null)
+  private_subnets =lookup(lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null),"app", null), "subnets", null), "id", null)
 }
