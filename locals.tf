464a3a12dfc868ade9c8ce77714ec0cfb9d802e7
vamsi113 <krishultimate5@gmail.com>
2023-06-05 13:01:50 -0400

    change

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   app_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null),"app", null), "subnets", null)
   database_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null),"database", null), "subnets", null)
   frontend_private_subnets = lookup(lookup(lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null),"frontend", null), "subnets", null)
-  public_subnets = lookup({for k, v in module.vpc.private_subnets : "subnets" => v.subnets}, "subnets", null)
+  public_subnets = lookup({for k, v in module.vpc.public_subnets : "subnets" => v.subnets}, "subnets", null)
 }
 
 #output "app_private_subnets" {
