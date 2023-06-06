4a360d22bea56b5b7fa724b3f3d6c72c1b97f113
vamsi113 <krishultimate5@gmail.com>
2023-06-06 11:45:16 -0400

    change

--- main.tf
+++ main.tf
@@ -44,5 +44,5 @@ module "vpc" {
 #}
 
 output "app_subnets" {
-  value = lookup(lookup(lookup(module.vpc, "private_subnets",null),"app",null),"subnets",null)[8].id
+  value = lookup(lookup(lookup(module.vpc, "private_subnets",null),"app",null),"subnets",null)
 }
