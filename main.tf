a56120220d65d2f809ffac6f24df1ad443cc13df
vamsi113 <krishultimate5@gmail.com>
2023-06-05 22:05:10 -0400

    change

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@
 #}
 
 module "vpc" {
-  for_each              = var.vpc
-  source                = "./vendor/modules/vpc"
-  cidr_block            = each.value.cidr_block
-  additional_cidr_block = each.value.additional_cidr_block
-  private_subnets = each.value.private_subnets
-  public_subnets = each.value.public_subnets
+  for_each                  = var.vpc
+  source                    = "./vendor/modules/vpc"
+  cidr_block                = each.value.cidr_block
+  additional_cidr_block     = each.value.additional_cidr_block
+  private_subnets           = each.value.private_subnets
+  public_subnets            = each.value.public_subnets
   subnet_availability_zones = each.value.subnet_availability_zones
-  env                   = var.env
-  management_vpc        = var.management_vpc
+  env                       = var.env
+  management_vpc            = var.management_vpc
 }
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "vpc" {
-#  value = module.vpc
-#}
-#
+output "vpc" {
+  value = module.vpc
+}
+
 ##output "subnets" {
 #  value = module.vpc.subnets
 #}
