0f7de7e499dc38714c42339f57e699e8c8b13912
vamsi113 <krishultimate5@gmail.com>
2023-06-06 22:54:43 -0400

    change

--- main.tf
+++ main.tf
@@ -95,6 +95,6 @@ module "app" {
   name= each.key
   max_size = each.value.max_size
   min_size = each.value.min_size
-  vpc_id   = element([for i,j in module.vpc: j.vpc_id],0)
+  vpc_id   = element([for i,j in module.vpc: j.vpc_id], 0)
   BASTION_NODE = var.BASTION_NODE
 }
